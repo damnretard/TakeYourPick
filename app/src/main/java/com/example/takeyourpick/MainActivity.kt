169e3ed3d67910888c4bf11d4e053a684487e9e3
Mansi Gupta <miamortinta@outlook.com>
2022-06-15 16:27:30 +0530

    first commit

---
+++ app/src/main/java/com/example/takeyourpick/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.takeyourpick
+
+import android.graphics.Color
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button1 = findViewById<Button>(R.id.button1)
+        val button2 = findViewById<Button>(R.id.button2)
+        val bgg = findViewById<androidx.constraintlayout.widget.ConstraintLayout>(R.id.bgg)
+
+        button1.setOnClickListener {
+            val leftNum = button1.text.toString().toInt()
+            val rightNum = button2.text.toString().toInt()
+            if (leftNum > rightNum){
+                bgg.setBackgroundColor(Color.GREEN)
+                Toast.makeText(this, "Correct!", Toast.LENGTH_SHORT).show()
+            } else {
+                bgg.setBackgroundColor(Color.RED)
+                Toast.makeText(this, "Wrong", Toast.LENGTH_SHORT).show()
+            }
+        }
+        button2.setOnClickListener {
+            val leftNum = button1.text.toString().toInt()
+            val rightNum = button2.text.toString().toInt()
+            if (leftNum < rightNum){
+                bgg.setBackgroundColor(Color.GREEN)
+                Toast.makeText(this, "Correct!", Toast.LENGTH_SHORT).show()
+            } else {
+                bgg.setBackgroundColor(Color.RED)
+                Toast.makeText(this, "Wrong", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+
+
+    }
+}
